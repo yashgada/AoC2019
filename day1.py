d667b96026e1a582e7bd0605df300d2fbfc490e6
Yash Gada <gadayash9@gmail.com>
2020-07-04 20:04:01 +0530

    Day 1 Solved

--- day1.py
+++ day1.py
@@ -1,2 +1,18 @@
-a=5
-print(a)
+with open("day1input.txt") as f:
+    raw = f.readlines()
+lines=[int(x.strip()) for x in raw]
+fuel=totalFuel=0
+for line in lines:
+    fuel+=line//3-2
+    
+def calcTotalFuel(weight):
+    total=0
+    while weight>8:
+        weight=weight//3-2
+        total+=weight
+    return total
+        
+for line in lines:
+    totalFuel+=calcTotalFuel(line)
+print(fuel)
+print(totalFuel)
